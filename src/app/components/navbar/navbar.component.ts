da4a76dfc03c66595526686b38ad1840aa83974b
Spagetka98 <lukasrichter149@gmail.com>
2023-09-26 14:58:50 +0200

    Add logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/api/auth.service';
+import { StorageService } from 'src/app/services/storage/storage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isLoading: boolean = false;
+
+  constructor(
+    private _auth: AuthService,
+    private _storage: StorageService,
+    private _router: Router){}
   
+  logout(): void {
+    
+    this.isLoading = true;
+    
+    this._auth.logout()
+    .subscribe()
+    .add(() => this.handleFinish());
+  }
+
+  private handleFinish(): void{
+    this.isLoading = false
+
+    this._storage.cleanStorage()
+    this._router.navigate(['login']);
+  }
 }
--- src/app/services/api/api.path.ts
+++ src/app/services/api/api.path.ts
@@ -8,11 +8,13 @@ const SECURED = '/secured';
 //Controller path
 const AUTH = '/auth';
 const TOKEN = '/token';
+const USER = "/user";
 const PASSWORD = "/password";
 
 export const LoginApi = BASE_API + PUBLIC + AUTH + '/login';
 export const RegisterApi = BASE_API + PUBLIC + AUTH + '/register';
-export const LogoutApi = BASE_API + PUBLIC + AUTH + '/logout';
+
+export const LogoutApi = BASE_API + SECURED + USER + '/logout';
 
 export const RefreshJWTApi = BASE_API + PUBLIC + TOKEN + '/refreshJWT';
 
